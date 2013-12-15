ec4aef21a795b52c877038d620172b6dac5b37ea
FA <otpdeviza@eyan.eu>
2013-12-15 11:04:31 +0100

    pmd

--- src/main/java/eu/eyan/srteditor/gui/controller/SrtEditorController.java
+++ src/main/java/eu/eyan/srteditor/gui/controller/SrtEditorController.java
@@ -13,9 +13,9 @@ import eu.eyan.srteditor.gui.view.SrtEditorView;
 
 public class SrtEditorController implements IController
 {
-    private SrtController srtRightController = new SrtController("hu.srt");
-    private SrtController srtLeftController = new SrtController("en.srt");
-    private SrtEditorView view = new SrtEditorView();
+    private final SrtController srtRightController = new SrtController("hu.srt");
+    private final SrtController srtLeftController = new SrtController("en.srt");
+    private final SrtEditorView view = new SrtEditorView();
 
     public SrtEditorController()
     {
@@ -30,7 +30,7 @@ public class SrtEditorController implements IController
         view.savebutton.addActionListener(new ActionListener()
         {
             @Override
-            public void actionPerformed(ActionEvent e)
+            public void actionPerformed(final ActionEvent e)
             {
                 srtRightController.save();
                 srtLeftController.save();
@@ -40,7 +40,7 @@ public class SrtEditorController implements IController
         view.copyTimesFromLeftToRightButton.addActionListener(new ActionListener()
         {
             @Override
-            public void actionPerformed(ActionEvent e)
+            public void actionPerformed(final ActionEvent e)
             {
                 copyTimes(srtLeftController.getList(), srtRightController.getList());
                 srtRightController.refresh();
@@ -51,7 +51,7 @@ public class SrtEditorController implements IController
         view.copyTimesFromRightToLeftButton.addActionListener(new ActionListener()
         {
             @Override
-            public void actionPerformed(ActionEvent e)
+            public void actionPerformed(final ActionEvent e)
             {
                 copyTimes(srtRightController.getList(), srtLeftController.getList());
                 srtLeftController.refresh();
@@ -65,7 +65,7 @@ public class SrtEditorController implements IController
         return view.getComponent();
     }
 
-    private void copyTimes(List<Srt> from, List<Srt> to)
+    private void copyTimes(final List<Srt> from, final List<Srt> to)
     {
         if (from.size() != to.size())
         {
--- src/main/java/eu/eyan/srteditor/parser/SrtParser.java
+++ src/main/java/eu/eyan/srteditor/parser/SrtParser.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-
 import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
@@ -14,55 +13,66 @@ import com.google.common.io.Files;
 
 import eu.eyan.srteditor.data.Srt;
 
-public class SrtParser {
+public class SrtParser
+{
 
-	public static List<Srt> parse(String path) {
-		List<Srt> ret = newArrayList();
-		try {
-			List<String> lines = Files.readLines(new File(path), Charsets.ISO_8859_1);
-			int actualLine = 0;
-			while(actualLine < lines.size()){
-				int srtNumber = Integer.parseInt(lines.get(actualLine++));
-				String srtTime = lines.get(actualLine++);
-				List<String> srtLines = newArrayList();
-				while(!Strings.isNullOrEmpty(lines.get(actualLine)))
-				{
-					srtLines.add(lines.get(actualLine++));
-				}
-				actualLine++;
-				
-				ret.add(new Srt(srtNumber, srtTime, srtLines));
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return ret;
-	}
+    public static List<Srt> parse(final String path)
+    {
+        List<Srt> ret = newArrayList();
+        try
+        {
+            List<String> lines = Files.readLines(new File(path), Charsets.ISO_8859_1);
+            int actualLine = 0;
+            while (actualLine < lines.size())
+            {
+                int srtNumber = Integer.parseInt(lines.get(actualLine++));
+                String srtTime = lines.get(actualLine++);
+                List<String> srtLines = newArrayList();
+                while (actualLine < lines.size() && !Strings.isNullOrEmpty(lines.get(actualLine)))
+                {
+                    srtLines.add(lines.get(actualLine++));
+                }
+                actualLine++;
 
-	public static void save(String path, List<Srt> list) {
-		try {
-			final File file = new File("_"+path);
-			if(file.exists())
-			{
-				Files.deleteRecursively(file);
-			}
-			for (final Srt srt : list) {
-				final String separator = "\r\n";
-				final String text = new StringBuilder()
-				.append(srt.getIndex())
-				.append(separator)
-				.append(srt.getTime())
-				.append(separator)
-				.append(Joiner.on(separator).join(srt.getLines()))
-				.append(separator)
-				.append(separator)
-				.toString();
-				Files.append(text, file, Charsets.ISO_8859_1);
-			}
-		} catch (final IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+                ret.add(new Srt(srtNumber, srtTime, srtLines));
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    public static void save(final String path, final List<Srt> list)
+    {
+        try
+        {
+            final File file = new File("_" + path);
+            if (file.exists())
+            {
+                Files.deleteRecursively(file);
+            }
+            for (final Srt srt : list)
+            {
+                final String separator = "\r\n";
+                final String text = new StringBuilder()
+                        .append(srt.getIndex())
+                        .append(separator)
+                        .append(srt.getTime())
+                        .append(separator)
+                        .append(Joiner.on(separator).join(srt.getLines()))
+                        .append(separator)
+                        .append(separator)
+                        .toString();
+                Files.append(text, file, Charsets.ISO_8859_1);
+            }
+        }
+        catch (final IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
